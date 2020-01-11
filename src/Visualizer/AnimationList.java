f3ee3e65611ca0ebe93d55e64b266b9451f1422d
christopher oosthuizen <Christopher.jan.Oosthuizen@gmail.com>
2020-01-10 19:42:19 -0600

    added a few comments and moved the animationList to inside the animationList

--- src/Visualizer/AnimationList.java
+++ src/Visualizer/AnimationList.java
@@ -8,61 +8,41 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class AnimationList extends ArrayList<Integer> {
-	SortAnimation ani;
+	SortAnimation animation;
 
 	public AnimationList() {
 		super();
-		ani = new SortAnimation(this);
+		animation = new SortAnimation(this);
 	}
 
 	public AnimationList(Collection<Integer> c) {
 		super(c);
-		ani = new SortAnimation(this);
+		animation = new SortAnimation(this);
 	}
-
-	public void play(int delay) throws InterruptedException {
-
-		ani.documentChange(this, -1);
-		ani.play(delay);
-
-	}
-	public boolean isRunning() {
-		return ani.isRewindRunning()||ani.isPlayRunning();
-	}
-	public boolean isPlaying() {
-		return ani.isPlayRunning();
-	}
-	public boolean isRewindRunning() {
-		return ani.isRewindRunning();
-	}
-
-	public void rewind(int delay) throws InterruptedException {
-
-		ani.documentChange(this, this.size());
-		ani.rewind(delay);
-
-	}
-
+	
+	
+	
 	@Override
 	public Integer get(int index) {
-		// ani.documentChange(this, index);
+		animation.documentChange(this, index);
 		return super.get(index);
 	}
 
 	@Override
 	public boolean add(Integer e) {
 		super.add(e);
-		ani.documentChange(this, this.size() - 1);
+		animation.documentChange(this, this.size() - 1);
 		return (true);
 	}
 
 	@Override
 	public Integer set(int index, Integer e) {
 		super.set(index, e);
-		ani.documentChange(this, index);
+		animation.documentChange(this, index);
 		return e;
 	}
 
@@ -70,37 +50,211 @@ public class AnimationList extends ArrayList<Integer> {
 	public Integer remove(int index) {
 		Integer temp = this.get(index);
 		super.remove(index);
-		ani.documentChange(this, 0);
+		animation.documentChange(this, 0);
 		return temp;
 
 	}
 
+	
+	
 	public void swap(int a, int b) {
 		int temp = this.get(a);
 		this.set(a, this.get(b));
 		this.set(b, temp);
 		int[] change = { a, b };
-		ani.documentChange(this, b);
+		animation.documentChange(this, b);
 	}
 
+	
+	
 	public SortAnimation getAnimation() {
-		return ani;
+		return animation;
 	}
 
 	public void setBounds(int x, int y, int width, int height) {
-		ani.setBounds(x, y, width, height);
+		animation.setBounds(x, y, width, height);
+	}
+	//tests if the play forward or rewind is on
+	public boolean isRunning() {
+		return animation.isRewindRunning() || animation.isPlayRunning();
+	}
+	//returns is is playing forward
+	public boolean isPlaying() {
+		return animation.isPlayRunning();
+	}
+	//checks if rewind is playing
+	public boolean isRewindRunning() {
+		return animation.isRewindRunning();
+	}
+
+	public void play(int delay) throws InterruptedException {
+
+		animation.documentChange(this, -1);
+		animation.play(delay);
+
+	}
+
+	public void rewind(int delay) throws InterruptedException {
+
+		animation.documentChange(this, this.size());
+		animation.rewind(delay);
+
 	}
 
 	public void pause() {
-		ani.pause();
+		animation.pause();
 	}
 
 	public void frameUp() {
-		ani.frameUp();
+		animation.frameUp();
 	}
 
 	public void frameDown() {
+		animation.frameDown();
+	}
+		//main animation class
+		private class SortAnimation extends JPanel {
 
+			private ArrayList<Integer> list;
+			private ArrayList<ArrayList<Integer>> changes;
+			private ArrayList<Integer> changesSpots;
+			private int selectedSpot;
+			private Timer forwardTimer;
+			private Timer backwardTimer;
+			private AtomicInteger times;
+
+			public void paint(Graphics g) {
+				super.paint(g);
+				int height = this.getHeight();
+
+				int spaceY = (int) (this.getHeight());
+
+				int spaceX = (int) ((this.getWidth()) - spaceY) / (list.size() - 1);
+				int spacersX = this.getWidth() / list.size();
+				int oneUnit = 1 + ((int) (height - (height * 0.07))) / list.stream().max((o1, o2) -> o1 - o2).get();
+
+				for (int i = 0; i < list.size(); i++) {
+
+					int size = list.get(i) * oneUnit * (int) (height * 0.003);
+					if (i != selectedSpot)
+						g.drawRect((spaceX) + i * spacersX, spaceY - size, spacersX, size);
+					else
+						g.fillRect((spaceX) + i * spacersX, spaceY - size, spacersX, size);
+
+				}
+
+			}
+
+			public SortAnimation(AnimationList list) {
+				this.selectedSpot = 0;
+
+				times = new AtomicInteger();
+				this.list = new ArrayList<Integer>(list);
+				this.changes = new ArrayList<ArrayList<Integer>>();
+				this.changesSpots = new ArrayList<Integer>();
+
+			}
+
+			public synchronized void play(int delay) throws InterruptedException {
+
+				forwardTimer = new Timer(delay, new ActionListener() {
+
+					@Override
+
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						if (forwardTimer != null || !backwardTimer.isRunning()) {
+							if (times.get() == changes.size()) {
+								forwardTimer.stop();
+
+							} else
+								frameUp();
+						}
+
+					}
+				});
+				forwardTimer.start();
+
+			}
+
+			public synchronized void rewind(int delay) {
+
+				backwardTimer = new Timer(delay, new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+
+						if (backwardTimer != null || !forwardTimer.isRunning()) {
+							if (times.get() == 0) {
+								backwardTimer.stop();
+
+							} else
+								frameDown();
+						}
+					}
+				});
+
+				backwardTimer.start();
+
+			}
+
+			public void pause() {
+				backwardTimer.stop();
+				forwardTimer.stop();
+
+			}
+
+			public void start() {
+				backwardTimer.start();
+			}
+
+			public void frameUp() {
+				list = changes.get(times.get());
+				selectedSpot = changesSpots.get(times.get());
+				repaint();
+				times.incrementAndGet();
+
+			}
+
+			public void frameDown() {
+				list = changes.get(times.get());
+				selectedSpot = changesSpots.get(times.get());
+				repaint();
+				times.decrementAndGet();
+			}
+
+			private void swap(int[] a) {
+				int temp = (int) list.get(a[0]);
+				list.set(a[0], list.get(a[1]));
+				list.set(a[1], temp);
+			}
+
+			public boolean isPlayRunning() {
+				return forwardTimer.isRunning();
+			}
+
+			public boolean isRewindRunning() {
+				return backwardTimer.isRunning();
+			}
+
+			public ArrayList<Integer> changes(int change) {
+				ArrayList<Integer> changers = new ArrayList<Integer>();
+				ArrayList<Integer> setOne = changes.get(change);
+				ArrayList<Integer> setTwo = changes.get(change - 1);
+				for (int i = 0; i < changes.get(change).size(); i++) {
+					if (!setOne.get(i).equals(setTwo.get(i)))
+						changers.add(i);
+				}
+				return changers;
+
+			}
+
+			public void documentChange(AnimationList lister, int selectedSpot) {
+				changes.add(new ArrayList<Integer>(lister));
+				changesSpots.add(selectedSpot);
+
+			}
+		}
 	}
 
-}
